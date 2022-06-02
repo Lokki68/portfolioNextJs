8413b70c9b570e8674378117b45422a9666c5e2b
Brice <bricel67@gmail.com>
2022-06-02 19:58:51 +0200

    Header

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {Section, SectionText, SectionTitle} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {LeftSection} from './HeroStyles';
+import {useRouter} from "next/router";
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
-);
+
+const Hero = () => {
+  const router = useRouter()
+
+  return (
+
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Bienvenue sur <br />
+          mon Portfolio
+        </SectionTitle>
+        <SectionText>
+          Je m'appelle Brice, je suis développeur FullStack. En reconversion professionnelle.
+        </SectionText>
+        <Button onClick={() => router.push('mailto:bricel67@gmail.com')}>En savoir plus</Button>
+      </LeftSection>
+    </Section>
+
+  );
+}
 
 export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,16 @@ import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
-import { Layout } from '../layout/Layout';
-import { Section } from '../styles/GlobalComponents';
+import {Layout} from '../layout/Layout';
+import {Section} from '../styles/GlobalComponents';
 
 const Home = () => {
   return (
     <Layout>
-      {/* <Section grid> */}
+      <Section grid>
         <Hero />
-        {/* <BgAnimation /> */}
-      {/* </Section> */}
+        <BgAnimation />
+      </Section>
       <Projects />
       <Technologies />
       <Timeline />
